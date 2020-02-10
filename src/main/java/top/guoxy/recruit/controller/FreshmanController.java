968bea85e55896761afe46df6d0f26c64463272b
guoxinyu <gxylong@126.com>
2020-02-10 16:51:48 +0800

    :art: 改用MyBatis

--- src/main/java/top/guoxy/recruit/controller/FreshmanController.java
+++ src/main/java/top/guoxy/recruit/controller/FreshmanController.java
@@ -2,10 +2,8 @@ package top.guoxy.recruit.controller;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
-import top.guoxy.recruit.model.FreshmanModel;
+import org.springframework.web.bind.annotation.*;
+import top.guoxy.recruit.model.Freshman;
 import top.guoxy.recruit.service.FreshmanService;
 import top.guoxy.recruit.utils.Result;
 import top.guoxy.recruit.utils.ResultUtil;
@@ -17,14 +15,21 @@ public class FreshmanController {
     @Autowired
     private FreshmanService service;
 
-    @GetMapping("/freshman/all")
-    public Result freshmanList() {
-         List<FreshmanModel> list = service.getFreshmanList(0);
-         return ResultUtil.success(list);
-    }
+
     @GetMapping("/freshman/{id}")
     public  Result freshman(@PathVariable("id") int id ) throws Exception {
-        FreshmanModel freshmanModel = service.getFreshmanByID(id);
-        return ResultUtil.success(freshmanModel);
+        Freshman freshman = service.getFreshmanByID(id);
+        return ResultUtil.success(freshman);
+    }
+
+    @PostMapping("/freshman")
+    public Result save(@RequestBody Freshman freshman) {
+        System.out.println(freshman.toString());
+        int status = service.insert(freshman);
+        if (status == 1) {
+            return ResultUtil.success("");
+        }else {
+            return ResultUtil.fail(-1,"请求失败，稍后再试");
+        }
     }
 }
--- src/main/java/top/guoxy/recruit/service/FreshmanService.java
+++ src/main/java/top/guoxy/recruit/service/FreshmanService.java
@@ -2,27 +2,31 @@ package top.guoxy.recruit.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import top.guoxy.recruit.model.FreshmanModel;
-import top.guoxy.recruit.repository.FreshmanRepository;
+import top.guoxy.recruit.dao.FreshmanMapper;
+import top.guoxy.recruit.model.Freshman;
 
+
+import java.sql.Timestamp;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
 @Service
 public class FreshmanService {
     @Autowired
-    private FreshmanRepository repository;
+    private FreshmanMapper mapper;
 
-    public List<FreshmanModel> getFreshmanList(int groupId) {
-        if (groupId==0) {
-            return repository.findAll();
-        } else {
-            return repository.findAllByGroupId(groupId);
-        }
+
+    public Freshman getFreshmanByID(int ID) throws Exception {
+        Freshman freshman = mapper.selectByPrimaryKey(ID);
+        return freshman;
     }
-    public FreshmanModel getFreshmanByID(int ID) throws Exception {
-        Optional<FreshmanModel> optional = repository.findById(ID);
-        return optional.orElseGet(FreshmanModel::new);
+
+    public int insert(Freshman freshman) {
+        freshman.setTime(new Timestamp(new Date().getTime()));
+        System.out.println(new Timestamp(new Date().getTime()));
+        return mapper.insert(freshman);
+
     }
 
 }
